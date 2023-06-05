76b778411fe98f4b99d7901e61dc9a0b4b762a33
wangyongfeng <1084415961@qq.com>
2023-06-05 17:36:16 +0800

    feat: init

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 800,
+
+		height: 600,
+	}); // 加载应用----适用于 react 项目
+
+	win.loadURL("http://localhost:3000/");
+}
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on("activate", () => {
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
+});
